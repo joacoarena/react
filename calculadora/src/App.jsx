dd40b5c822c63c93ebdf4832cab446b334627444
joacoarena <joaquin.t.arena@gmail.com>
2023-07-13 16:53:25 -0300

    input styles

--- calculadora/src/App.jsx
+++ calculadora/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import Boton from './componentes/botones'
 import './App.css'
-
+import Pantalla from './componentes/Pantalla'
 function App() {
 
   return (
@@ -11,12 +11,31 @@ function App() {
           <img src="" alt="" className='freecodecamp-logo' />
         </div>
         <div className='contenedor-calculadora'>
+          <Pantalla/>
           <div className='fila'>
             <Boton>1</Boton>
+            <Boton>2</Boton>
+            <Boton>3</Boton>
+            <Boton>+</Boton>
           </div>
-          <div className='fila'></div>
-          <div className='fila'></div>
-          <div className='fila'></div>
+          <div className='fila'>
+          <Boton>4</Boton>
+          <Boton>5</Boton>
+          <Boton>6</Boton>  
+          <Boton>-</Boton>
+          </div>
+          <div className='fila'>
+          <Boton>7</Boton>
+          <Boton>8</Boton>
+          <Boton>9</Boton>
+          <Boton>*</Boton>
+          </div>
+          <div className='fila'>
+          <Boton>=</Boton>
+          <Boton>0</Boton>
+          <Boton>.</Boton>
+          <Boton>/</Boton>
+          </div>
           <div className='fila'></div>
         </div>
      </div>
