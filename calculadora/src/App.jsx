cc082082b02510c6bbb026e1f8a9642c08e90a6e
joacoarena <joaquin.t.arena@gmail.com>
2023-07-13 16:02:15 -0300

    start app

---
+++ calculadora/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='App'>
+        <div className='freecodecamp-logo-contenedor'>
+          <img src="" alt="" className='freecodecamp-logo' />
+        </div>
+        <div className='contenedor-calculadora'>
+          <div className='fila'></div>
+          <div className='fila'></div>
+          <div className='fila'></div>
+          <div className='fila'></div>
+          <div className='fila'></div>
+        </div>
+     </div>
+    </>
+  )
+}
+
+export default App
